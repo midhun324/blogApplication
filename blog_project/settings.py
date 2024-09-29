18e1b48cf7172dabafc04ba607e0dc5fe820f2ad
MidhunStreben <midhun.jayan@strebentechnik.com>
2024-09-29 13:25:38 +0530

    test

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(0in^65anwkled1ebiz#a0ybv-_)twd3nx)-i_u)jtc()(7lr%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
